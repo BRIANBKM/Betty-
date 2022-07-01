7c1350715b1d132adf6ea00987d1f44ca0db2586
Brian Maina <107069268+BRIANBKM@users.noreply.github.com>
2022-07-01 16:32:30 +0300

    v

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -12,14 +12,14 @@
 
 int main(void)
 {
-	int i = 122 ;
+	int i = 122;
 
 	while (i > 96)
 	{
-		putchar(i) ;
-		i-- ;
+		putchar(i);
+		i--;
 	}
-	putchar(10\n) ;
+	putchar(10);
 
-	return (0) ;
+	return (0);
 }
