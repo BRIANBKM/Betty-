14bcf21baf15cdb72e90bb02f73b56e739376b75
Brian Maina <107069268+BRIANBKM@users.noreply.github.com>
2022-07-01 16:35:39 +0300

    valiable

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,25 +1,5 @@
-#include <stdio.h>
+#include <studio.h>
 #include <stdlib.h>
-#include <time.h>
+#incluse <time.h>
 
 /**
- * main - main entry
- *
- * Description: print the lowercase  alphabet in reverse
- *
- * Return: Always 0 (Success)
- */
-
-int main(void)
-{
-	int i = 122;
-
-	while (i > 96)
-	{
-		putchar(i);
-		i--;
-	}
-	putchar(10);
-
-	return (0);
-}
