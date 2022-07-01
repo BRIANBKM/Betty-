08cce1d46e8700a057ee6b7876df2e8d78765885
Brian Maina <107069268+BRIANBKM@users.noreply.github.com>
2022-07-01 12:13:08 +0300

    variables

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,6 +13,7 @@
 int main (void)
 {
 	int n;
+	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n == 0)
@@ -21,6 +22,5 @@ int main (void)
 		printf("%d is negative/n", n);
 	else (n > 0)
 		printf("%d is positive/n", n);
-
 	return (0);
 }
