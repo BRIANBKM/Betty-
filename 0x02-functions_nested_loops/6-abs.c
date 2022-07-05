5ddbf3b44527b07a79d05af85e1f576eaf002545
Brian Maina <107069268+BRIANBKM@users.noreply.github.com>
2022-07-05 16:18:43 +0300

    functions

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - main entry
+ *
+ * Description:
+ *
+ * Return: integer
+ */
+
+int _abs(int b)
+{
+	if (b > 0)
+	{
+		return (b);
+	}
+	else if (b < 0)
+	{
+		return (-b);
+	}
+	else
+	{
+		return (b);
+	}
+}
