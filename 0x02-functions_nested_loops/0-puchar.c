2552873e292769f272a61871d896e6e6ec163984
BRIANBKM <brianmaina900@gmail.com>
2022-07-05 05:34:33 -0700

    functions

--- 0x02-functions_nested_loops/0-puchar.c
+++ 0x02-functions_nested_loops/0-puchar.c
@@ -1,13 +1,16 @@
 #include "main.h"
 
 /**
- * main- main entry 
+ * main- main entry
+ *
  * Description: print _putchar
- * Return: (0) 
- **/
+ *
+ * Return: (0) i
+ */
 
 int main(void)
 {
-	printf("_putchar/n");
+	printf("_putchar/");
+	printf("\n");
 	return (0);
 }
