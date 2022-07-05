a03e2bac5be356366795af219d929e6daef83e09
Brian Maina <107069268+BRIANBKM@users.noreply.github.com>
2022-07-05 19:06:33 +0300

    functions

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * prints all numbers from n to 98
+ * @int: type number 
+ * return: 0
+ */
+ 
+ void print_to_98(int n)
+ {
+        int n;
+        
+        while (n < 98)
+        {
+              _putchar(n, );
+              n++;
+        }
+          
