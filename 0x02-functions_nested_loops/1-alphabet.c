5dced581d635e16bba04637b7059dbc86d7c78f5
BRIANBKM <brianmaina900@gmail.com>
2022-07-05 05:50:46 -0700

    functions

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,21 @@
 #include "main.h"
+
+/*
+ * main - entry 
+ *
+ * description:
+ *
+ * Return: end
+ */
+
+int main(void)
+{
+	char i = 97;
+	while (i<122)
+	{
+	putchar("i");
+	i++;
+	}
+	putchar("\n");
+	return (0);
+}
