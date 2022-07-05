f02bfb08829b68bef21778dee5a900e6eb536b17
Brian Maina <107069268+BRIANBKM@users.noreply.github.com>
2022-07-05 18:19:21 +0300

    functions

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,19 +1,15 @@
 #include "main.h"
-
 /**
- * main - computes the absolute value of an interger
- * Description:computes the absolute value of an interger.
- * Return: n (positive) and -n (negative)
+ * _abs - compute the absolute value of an integer
+ * @n: int type number
+ * Return: absolute value of @n
  */
-
 int _abs(int n)
 {
-	if (n > 0)
-	{
-		return (n);
-	}
-	else  (n < 0)
+	if (n < 0)
 	{
 		return (n * -1);
-	
-}
+	}
+	else
+	{
+		return (n);
