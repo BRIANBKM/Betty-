0599c4723b3de78190fa115ee4da43056d41e3e5
BRIANBKM <brianmaina900@gmail.com>
2022-07-06 09:59:01 -0700

    debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,10 +1,11 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio>
 
 void positive _or_negative(int i);
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
+void ptint_remaining_days(int month, int day, int year);
 
 #endif /* MAIN.H */ 
