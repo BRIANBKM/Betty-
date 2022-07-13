396221ac9d9f93a8879d6614707caf02464f84db
Brian Maina <107069268+BRIANBKM@users.noreply.github.com>
2022-07-13 16:06:09 +0300

    pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * *_strcat - concatenates two strings
- * @dest - pointer to a char
- * @src - pointer to a char
+ * *_strcat - function commute strings
+ * @dest: pointer to a char
+ * @src: pointer to a char
  * Return: pointer to resulting string
  */
 char *_strcat(char *dest, char *src)
@@ -26,4 +26,3 @@ char *_strcat(char *dest, char *src)
 	dest[i] = '\0';
 	return (dest);
 }
-
