43fa30c71973344438479126432690f438d5c1bc
Brian Maina <107069268+BRIANBKM@users.noreply.github.com>
2022-07-13 19:53:27 +0300

    pointers

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,14 +6,14 @@
  */
 char *string_toupper(char *s)
 {
-        input count = o;
-        
-        while (*(s + count) != '\0')
-        {
-                 if ((*(s + count) >= 97) && (*(s + count) <= 122))
-                          *(s + count) = *(s + count) - 32;
-                 count++;
-        }
-        
-        return (s)
+	int count = 0;
+
+	while (*(s + count) != '\0')
+	{
+		if ((*(s + count) >= 97) && (*(s + count) <= 122))
+			*(s + count) = *(s + count) - 32;
+		count++;
+	}
+
+	return (s);
 }
