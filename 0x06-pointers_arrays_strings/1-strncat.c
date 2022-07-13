7baaa523068d2b0531e2b5df79e497f8260c54ba
BRIANBKM <brianmaina900@gmail.com>
2022-07-13 06:03:13 -0700

    pointers

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: string one 
+ * @src: strng two
+ * @n: interger
+ * Return: 0
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -16,6 +16,5 @@ void print_numbers(int n);
 char *infinite_add(char *n1, char *n2, char*r, int size_r);
 void print_buffer(char *b, int size);
 
-
 #endif
 
