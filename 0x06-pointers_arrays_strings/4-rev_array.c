2f95e80448bddf8a1c2ca0b916cc7cd9c1bb3724
Brian Maina <107069268+BRIANBKM@users.noreply.github.com>
2022-07-13 19:52:24 +0300

    ponters

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -6,17 +6,17 @@
  * @n: number of elements in an array
  * return: 0
  */
- void reverse_array(int *a, int n)
- {
-        int i, j, temp;
-        
-        for i = 0; i < n - 1; i++)
-        {
-                for (j = i + i; j > 0; j--)
-                {
-                        temp = *(a + j);
-                        *(a + j) = *(a + (J - 1));
-                        *(a + (j - 1)) = temp;
-                }
-        }
- }
+void reverse_array(int *a, int n)
+{
+	int i, j, temp;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(a + j);
+			*(a + j) = *(a + (j - 1));
+			*(a + (j - 1)) = temp;
+		}
+	}
+}
