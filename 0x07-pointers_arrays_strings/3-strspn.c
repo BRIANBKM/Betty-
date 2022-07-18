760e31632f5e2d246fba1b4674c4fb8c1e428145
Brian Maina <107069268+BRIANBKM@users.noreply.github.com>
2022-07-18 21:16:07 +0300

    strings

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * _strspn - gets the length of a perfix substring
- * @s: the string
- * @accept: substring
- * Return: ponter to dest
+ * _strspn - gets the length of a prefix substring.
+ * @s: initial segment.
+ * @accept: accepted bytes.
+ * Return: the number of accepted bytes.
  */
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j, bool;
 
-	for (i = 0; *(s + i) != '\0'; j++)
+	for (i = 0; *(s + i) != '\0'; i++)
 	{
 		bool = 1;
 		for (j = 0; *(accept + j) != '\0'; j++)
@@ -18,6 +18,7 @@ unsigned int _strspn(char *s, char *accept)
 			if (*(s + i) == *(accept + j))
 			{
 				bool = 0;
+				break;
 			}
 		}
 		if (bool == 1)
