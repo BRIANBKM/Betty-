6c80122941f55b4f1b9289c8a93d5de00b8f7f0b
Brian Maina <107069268+BRIANBKM@users.noreply.github.com>
2022-07-18 20:10:04 +0300

    strings

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,9 +13,9 @@ unsigned int _strspn(char *s, char *accept)
 	for (i = 0; *(s + i) != '\0'; j++)
 	{
 		bool =1;
-		for (j = 0; *accept + j) != '\0'; j+=)
+		for (j = 0; *(accept + j) != '\0'; j+=)
 		{
-			if (*(s + i) == *(accept + J))
+			if (*(s + i) == *(accept + j))
 			{
 				bool = 0;
 			}
