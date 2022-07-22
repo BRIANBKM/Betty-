b8823a53509db5e16fba4694ca595b3ef2d9fe6e
Brian Maina <107069268+BRIANBKM@users.noreply.github.com>
2022-07-22 13:12:54 +0300

    arguments

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,40 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+#include <ctype.h>
 /**
- * main - prints the minimum number of coins to make change for
- * an amount of money
- * @argc: number of command line arguments
- * @argv: array that contain the program command line arguments
- * return: (0)
+ * main - adds positive numbers.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
 int main(int argc, char *argv[])
 {
+	int i, j, add = 0;
 
-	int cents, ncoins = 0;
-
-	if (argc == 1 || argc > 2)
+	for (i = 1; i < argc; i++)
 	{
-		printf("Error\n");
-		return (1);
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		add += atoi(argv[i]);
 	}
-
-	cents = atoi(argv[1]);
-
-	while (cents > 0)
-	{
-		if (cents > 0)
-			cents -= 25;
-		else if (cents >= 10)
-			cents -= 10;
-		else if (cents >= 5)
-			cents -= 5;
-		else if (cents >= 2)
-			cents -= 2;
-		else if (cents >= 1)
-			cents -= 1;
-		ncoins += 1;
-	}
-	printf("%d\n", ncoins);
+	printf("%d\n", add);
 	return (0);
 }
