e3187e84954eace38358a85969f4d114889c40e0
Brian Maina <107069268+BRIANBKM@users.noreply.github.com>
2022-07-25 18:41:47 +0300

    mallocs

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,22 +1,24 @@
 #include "main.h"
 /**
- * *str_concat - concatenates two strings
- * @s1: string one
- * @s2: string two 
- * return: NULL ON failure
+ * str_concat - concatenates two strings.
+ * @s1: first string.
+ * @s2: second string.
+ *
+ * Return: pointer of an array of chars
  */
 char *str_concat(char *s1, char *s2)
 {
 	char *strout;
 	unsigned int i, j, k, limit;
 
-	if  (s1 == NULL)
+	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
 	for (i = 0; s1[i] != '\0'; i++)
 		;
+
 	for (j = 0; s2[j] != '\0'; j++)
 		;
 
@@ -28,11 +30,11 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 	}
 
-	for (k = 0; k < i;k++)
+	for (k = 0; k < i; k++)
 		strout[k] = s1[k];
-	
+
 	limit = j;
-	for (j = 0;j <= limit;j++)
+	for (j = 0; j <= limit; k++, j++)
 		strout[k] = s2[j];
 
 	return (strout);
