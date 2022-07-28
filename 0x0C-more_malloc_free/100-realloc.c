7c5f5e6a99666a85c11124b573111ebec8ebeea9
Brian Maina <107069268+BRIANBKM@users.noreply.github.com>
2022-07-28 16:15:08 +0300

    malloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,14 +1,16 @@
 #include "main.h"
+#include <stdlib.h>
 /**
- * _realloc - reallocates a mwemory block
- * @ptr: pointer to the momory previously allocated
- * @old_size: size, in ni bytes, of the allocated space of ptr
- * @new_size: new size, in bytes, of the new mwmory block
- * return: ptr
- * if new-size == old_size, returns ptr without changes
- * if malloc fails, retuens NULL
+ * _realloc - reallocates a memory block.
+ * @ptr: pointer to the memory previously allocated.
+ * @old_size: size, in bytes, of the allocated space of ptr.
+ * @new_size: new size, in bytes, of the new memory block.
+ *
+ * Return: ptr.
+ * if new_size == old_size, returns ptr without changes.
+ * if malloc fails, returns NULL.
  */
-void *_realloc (void *ptr, unsigned int old_size, unsigned int new_size)
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	if (new_size == 0 && ptr != NULL)
 	{
@@ -16,11 +18,11 @@ void *_realloc (void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 
-	if (ptr = NULL)
+	if (ptr == NULL)
 		ptr = malloc(new_size);
 
 	if (new_size == old_size)
-		return(ptr);
+		return (ptr);
 
 	free(ptr);
 	ptr = malloc(new_size);
