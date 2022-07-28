225c71b8bc10dca9ef44c7a0cd3d848f11a5994a
Brian Maina <107069268+BRIANBKM@users.noreply.github.com>
2022-07-28 16:05:38 +0300

    mlloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,12 +18,12 @@
 	if (s2 == NULL)
 		s2 = "";
 
-
 	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
 		;
 
 	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
 		;
+
 	if (n > ls2)
 		n = ls2;
 
@@ -36,7 +36,11 @@
 
 	for (i = 0; i < lsout; i++)
 		if (i < ls1)
-			sout = s1[i];
+			sout[i] = s1[i];
+		else
+			sout[i] = s2[i - ls1];
+
 	sout[i] = '\0';
+
 	return (sout);
 }
